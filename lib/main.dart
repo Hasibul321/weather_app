19cfcbf7a3f3c54e1c38e9ea415b43290492331b
Himel <hasibulalam192@gmail.com>
2022-11-20 13:52:21 +0600

    Api calling is successful

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,47 @@ import 'package:dropdown_button2/dropdown_button2.dart';
 void main() {
   runApp(
     MaterialApp(
-      home:myApp(),
+      home:splashScreen(),
   ));
 }
+class splashScreen extends StatelessWidget {
+  const splashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('images/weather.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("HamroMasum",
+            style: TextStyle(
+              color: Colors.black54,
+            ),),
+            MaterialButton(
+          child: Text("Home Screen"),
+                color: Colors.blue,
+                
+                onPressed: (){
+            Navigator.push(context, 
+            MaterialPageRoute(builder: (context)=>myApp()),
+                  );
+                })
+          ],
+        ),
+      )
+    );
+  }
+}
+
+
 class myApp extends StatefulWidget {
   const myApp({Key? key}) : super(key: key);
 
@@ -19,8 +57,9 @@ class myApp extends StatefulWidget {
   State<myApp> createState() => _myAppState();
 }
 
+
 class _myAppState extends State<myApp> {
-  List cityList=['naogaon','dhaka','bogra','rajshahi'];
+  List cityList=['Naogaon','Dhaka','Bogra','Rajshahi'];
   String?selectedValue;
   var temp,humidity,lat,lon,local,city,pressure,sunset,sunrise,visibility;
   //
@@ -38,36 +77,27 @@ class _myAppState extends State<myApp> {
   }
 
   void getLocation()async{
-    String url='https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&units=metric&appid=56c6ebf76fffc33698398004b9dbf5cb';
+   String url = "http://api.weatherapi.com/v1/forecast.json?key=23e30e09626240998a133605222011 &q="+lat+","+lon+"&days=7&aqi=yes&alerts=no";
     var res=await http.get(Uri.parse(url));
     var result=jsonDecode(res.body);
+    //print(result['location']['name']);
     setState(() {
-      temp=result['main']['temp'];
-      city=result['name'];
-      humidity = result['main']['humidity'];
-      pressure=result['main']['pressure'];
-      sunset=result['sys']['sunset'];
-      sunrise=result['sys']['sunrise'];
-      visibility=result['visibility'];
+      temp=result['current']['temp_c'];
+      city=result['location']['name'];
+
     });
   }
 
   TextEditingController textEditingController=new TextEditingController();
    void getWeather()async{
-     String url="https://api.openweathermap.org/data/2.5/weather?q=$city&units=metric&appid=56c6ebf76fffc33698398004b9dbf5cb&fbclid=IwAR067N0t4vM8F_wu6GTcctuTFm4r5OddMkqVjf32L_CqyCG6TqkBhUx0Rg0";
+     String url = "http://api.weatherapi.com/v1/forecast.json?key=23e30e09626240998a133605222011 &q="+city+"&days=7&aqi=yes&alerts=no";
      var res=await http.get(Uri.parse(url));
     var result=jsonDecode(res.body);
     setState(() {
-      temp=result['main']['temp'];
-      humidity = result['main']['humidity'];
-      lat=result['coord']['lat'];
-      lon= result['coord']['lon'];
-      pressure=result['main']['pressure'];
-      sunset=result['sys']['sunset'];
-      sunrise=result['sys']['sunrise'];
-      visibility=result['visibility'];
-      // temp=result['main']['temp'];
-      // humidity = result['main']['humidity'];
+      temp=result['current']['temp_c'];
+      lat=result['location']['lat'];
+      lon= result['location']['lon'];
+
 
     });
   }
@@ -107,10 +137,10 @@ class _myAppState extends State<myApp> {
                 child: Row(
                   children: [
                     SizedBox(
-                      width:150,
+                      width:140,
                       child: SizedBox(
                         height:100,
-                        width:100,
+                        width:90,
 
                         child: DropdownButton2(
                           alignment: AlignmentDirectional.center,
@@ -157,7 +187,7 @@ class _myAppState extends State<myApp> {
                       },
                       child: Row(
                         children: [
-                          Text("location",
+                          Text("Location",
                             style: TextStyle(
                               fontSize: 20,
                               fontWeight: FontWeight.bold,
@@ -194,7 +224,7 @@ class _myAppState extends State<myApp> {
               ),
               Expanded(child: Row(
                 children: [
-                  Text("city name    "+city.toString(),
+                  Text("City name   "+city.toString(),
                     style: TextStyle(
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
@@ -223,7 +253,7 @@ class _myAppState extends State<myApp> {
               Text(dt,style: TextStyle(fontSize: 20,),),
               Expanded(child: Row(
                 children: [
-                  Text("তাপমাত্রা : ",
+                  Text("Temp : ",
                   style: TextStyle(
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
@@ -242,80 +272,7 @@ class _myAppState extends State<myApp> {
                     ),),
                 ],
               )),
-              Expanded(child: Row(
-                children: [
-                  Text("আর্দ্রতা  ",style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),),
-                  SizedBox(width:100,),
-                  Text( humidity.toString(),style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),)
-                ],
-              )),
-              Expanded(child: Row(
-                children: [
-                  Text("বায়ু চাপ",style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),),
-                  SizedBox(width:100,),
-                  Text(pressure!=null?pressure.toString()+" mBar":"loading",style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),)
-                ],
-              )),
-              Expanded(child: Row(
-                children: [
-                  Text("দৃষ্টিসীমা",style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),),
-                  SizedBox(width:100,),
-                  Text( visibility!=null?(visibility/1000).toString()+" km":"Loading",style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),)
-                ],
-              )),
-              Expanded(child: Row(
-                children: [
-                  Text("সূর্যোদয় ",style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),),
-                  SizedBox(width:105,),
-                  Text( "সূর্যাস্ত",style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),)
-                ],
-              )),
-              Expanded(child: Row(
-                children: [
-                  Image.network('https://rukminim1.flixcart.com/image/416/416/jzsqky80/poster/h/y/4/medium-nature-wall-poster-sunrise-poster-poster-high-resolution-original-imafjqjyasudq8sg.jpeg?q=70'),
-                  SizedBox(width:90,),
-                  Image.network('https://media.istockphoto.com/id/1172427455/photo/beautiful-sunset-over-the-tropical-sea.jpg?s=612x612&w=0&k=20&c=i3R3cbE94hdu6PRWT7cQBStY_wknVzl2pFCjQppzTBg='),
-                ],
-              )),
-              Expanded(child: Row(
-                children: [
-                  Text( sunrise!=null?formatted(sunrise):"loading",style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),),
-                  SizedBox(width:40,),
 
-                  Text(sunset!=null?formatted(sunset):"loading",style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),),
-
-                ],
-              )),
 
             ],
           ),
